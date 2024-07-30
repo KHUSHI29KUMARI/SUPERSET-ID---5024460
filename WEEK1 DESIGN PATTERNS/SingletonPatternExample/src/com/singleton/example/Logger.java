8d24f5f3909065f6e4b7dad2dbf13561af8f9ea5
KHUSHI29KUMARI <khushkumari2911@gmail.com>
2024-07-30 11:47:48 +0530

    Add files via upload

---
+++ WEEK1 DESIGN PATTERNS/SingletonPatternExample/src/com/singleton/example/Logger.java
@@ -0,0 +1,28 @@
+package com.singleton.example;
+
+public class Logger {
+    // Private static instance of Logger
+    private static Logger instance;
+
+    // Private constructor to prevent instantiation
+    private Logger() {
+        // Initialization code, if any
+    }
+
+    // Public method to provide access to the instance
+    public static Logger getInstance() {
+        if (instance == null) {
+            synchronized (Logger.class) {
+                if (instance == null) {
+                    instance = new Logger();
+                }
+            }
+        }
+        return instance;
+    }
+
+    // Example logging method
+    public void log(String message) {
+        System.out.println("Log message: " + message);
+    }
+}
